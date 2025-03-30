bcdd3109a1c761d14d3b1cccfad482323d86cf93
MaxGitHub0515 <pavlovskymaxym4@gmail.com>
2025-03-31 00:08:28 +0200

    updated styling

--- 12-book-store/frontend/tailwind.config.js
+++ 12-book-store/frontend/tailwind.config.js
@@ -1,18 +1,25 @@
-// /** @type {import('tailwindcss').Config} */
-
-// export default {
-//     content: [
-//         "./index.html",
-//         "./src/**/*.{js,ts,jsx,tsx}",
-
-//     ],
-//     theme: {
-//       extend: {},
-//     },
-//     plugins: [],
-//   };
-  
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FFCE1A',
+        'secondary' : "#0D0842",
+        'blackBG': '#F3F3F3',
+        'Favorite': '#FF5841'
+      }, 
+      fontFamily: {
+        'primary' : ["Montserrat", "sans-serif"],
+        'secondary' : ["Nunito Sans", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
 
   // npm install -D autoprefixer postcss - legit more specific
   // npm install -D @tailwindcss/postcss ----> very specific - rarely used*
