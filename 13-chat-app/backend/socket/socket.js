a6734e1a5273d153e89509661156f49d5fa3f3cb
MaxGitHub0515 <pavlovskymaxym4@gmail.com>
2025-09-28 01:39:38 +0200

    backend & frontend: Added and Set-up socket.io server; /assets - added notification sound, updated /sidebar, /messages, /context, /hooks

--- 13-chat-app/backend/socket/socket.js
+++ 13-chat-app/backend/socket/socket.js
@@ -6,21 +6,33 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server);
-const userSocketMap = {}; 
+
+export const getRecieverSocketID = (receiverID) => {
+    return userSocketMap[receiverID]
+}
+// {userID : socketID}
+const userSocketMap = new Map() 
 
 // socket has various properties like id etc
-// io.on('connect', (socket) => {
-//     console.log("A User Is Connected", socket.id)
+io.on('connect', (socket) => {
+    console.log("A User Is Connected", socket.id);
 
-//     // socket.on() is used to listen to the events, it is used both for front and server-side
-//     socket.on('disconnect', () => {
-//         console.log("A User Was Disconnected");
-        
-//     })
+    // got into the query which is on the frontend
+    const userID = socket.handshake.query.userID;
+    if(userID != "undefined") userSocketMap[userID]
+    // tp send events tp all connected users
+    io.emit('getOnlineUsers', Object.keys(userSocketMap))
+
+    // socket.on() is used to listen to the events, it is used both for front and server-side
+    socket.on('disconnect', () => {
+        console.log("A User Was Disconnected");
+        delete userSocketMap[userID];
+        io.emit('getOnlineUsers', Object.keys(userSocketMap))
+    })
 
     
 
-// });
+});
 
 
 
