998bd33536c41d5f20a010225719f59175e5d4a6
MaxGitHub0515 <pavlovskymaxym4@gmail.com>
2025-09-30 12:22:42 +0200

    still fixing an testing after deploy

--- 13-chat-app/frontend/src/hooks/useListenMessages.js
+++ 13-chat-app/frontend/src/hooks/useListenMessages.js
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext"
 import useConversation from "../zustand/useConversation"
-import notificationSound from "../assets/sounds/buble-notification.wav"
+import notificationSound from "../assets/sounds/notification.mp3"
 
 const useListenMessages = () => {
   const {socket} = useSocketContext();
