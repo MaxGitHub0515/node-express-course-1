e1367468ec6cc79d7f8bf4c0c38b4dcdaa271a7e
MaxGitHub0515 <pavlovskymaxym4@gmail.com>
2025-10-14 02:04:09 +0200

    backend: auth added /models

---
+++ 14-e-commerce/backend/models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema =  new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },  
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"]
+    },
+    cartItems: [
+        {
+            quantity: {
+                type: Number,
+                default: 1,
+            },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+            }
+        }
+    ],
+    role: {
+        type: String,
+        enum: ["costumer", "admin"],
+        default: "customer"
+    }
+
+}, {timestamps: true});
+
+
+const User = mongoose.model("User", userSchema, "user")
+userSchemae.pre('save', async (next) => {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt)
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+userSchema.methods.comparePwd = async function(password) {
+    return await bcrypt.compare(password,  this.password)
+} 
+export default User;
+3142
