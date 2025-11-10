9d2fbfea6f384056fde9adf8009185c491e84828
Max Pavlovskyi <max@Maxs-MacBook-Pro.local>
2025-11-10 10:54:32 +0100

    Implement JWT authentication with access and refresh tokens, including secure cookie handling and Redis storage for refresh tokens.

--- 14-e-commerce/backend/controllers/auth.controller.js
+++ 14-e-commerce/backend/controllers/auth.controller.js
@@ -1,7 +1,28 @@
 
 import {StatusCodes} from "http-status-codes";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
 
+const generateTokens = (userId) => {
+     const accessToken = jwt.sign({userId}, process.env.JWT_ACCESS_SECRET, {
+          expiresIn: "15m"
+     });
+     const refreshToken = jwt.sign({userId}, process.env.JWT_REFRESH_SECRET, {
+          expiresIn: "7d"
+     });
+     return {accessToken, refreshToken};
+}
+const storeRefreshToken = async (userId, refreshToken) => {
+     await redis.set(`refresh_token:${userId}`, refreshToken, 'EX', 7 * 24 * 60 * 60); // 7 days expiration - store in redis
+}
+const setCookies = (res, accessToken, refreshToken) => {
+     res.cookie("accessToken", accessToken, {
+          httpOnly: true, // prevent XSS attacks
+          maxAge: 15 * 60 * 1000, // cookie lives 15 minutes
+          sameSite: "strict", // prevent CSRF attacks
+          secure: process.env.NODE_ENV === "production"
+})};
 export const SignUp = async (req, res) => {
      const {email, password, username} = req.body;
      try {
@@ -16,8 +37,18 @@ export const SignUp = async (req, res) => {
           email,
           password
      })
+     // authenticate
+     const {accessToken, refreshToken} = generateTokens(user._id);
+     await storeRefreshToken(user._id, refreshToken);
+     setCookies(res, accessToken, refreshToken);
+
      res.status(StatusCodes.CREATED).json({
-          user,
+          user: {
+               id: user._id,
+               username: user.username,
+               email: user.email,
+               role: user.role
+          },
           message: "User was created successfully"
      })
      } catch (error) {
--- 14-e-commerce/backend/models/user.model.js
+++ 14-e-commerce/backend/models/user.model.js
@@ -42,7 +42,7 @@ const userSchema =  new mongoose.Schema({
 
 
 
-userSchema.pre('save', async (next) => {
+userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     try {
         const salt = await bcrypt.genSalt(10);
