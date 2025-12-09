7d97272ce0b4c0589c57664a6b7ed9eb66164b86
MaxGitHub0515 <pavlovskymaxym4@gmail.com>
2025-12-09 19:44:07 +0100

    Swaped App logo

--- 13-chat-app/frontend/src/components/sidebar/Logo.jsx
+++ 13-chat-app/frontend/src/components/sidebar/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = () => {
     <div className="flex flex-col justify-center items-center ">
         <div className="">
             <Link to="/">
-            <img src="ic-logo-prev.svg"
+            <img src="app-logo.svg"
             className="logo w-32 hover:opacity-40 ease-in-out 
              duration-200  "  />
         </Link>
