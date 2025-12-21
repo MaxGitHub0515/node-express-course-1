4481f281746aed7ac798e468f8434bbeadd64f00
/ <105605213+MaxGitHub0515@users.noreply.github.com>
2025-12-21 23:55:02 +0100

    Another commit

--- portfolio-hosted/main-dashboard/rt-server/server.js
+++ portfolio-hosted/main-dashboard/rt-server/server.js
@@ -151,10 +151,7 @@ if (process.env.NODE_ENV === "localproduction") {
   app.use(express.static(clientBuildPath));
   
   // catch-all SPA fallback 
-  app.get('/*splat', (req, res, next) => {
-    if (req.url.startsWith('/api/v1/')) {
-      return next();
-    }
+  app.get(/(.*)/, (req, res) => {
     const tryPath = path.join(clientBuildPath, 'index.html');
     console.log(`Serving index.html fallback for: ${req.url.blue} from ${tryPath.cyan}`);
     res.sendFile(tryPath, (err) => {
