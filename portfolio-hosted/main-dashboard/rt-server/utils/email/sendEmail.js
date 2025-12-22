9ab10c94acb30d9a2c03f67f96718919c35189aa
/ <105605213+MaxGitHub0515@users.noreply.github.com>
2025-12-22 03:41:29 +0100

    Ongoing email fix

--- portfolio-hosted/main-dashboard/rt-server/utils/email/sendEmail.js
+++ portfolio-hosted/main-dashboard/rt-server/utils/email/sendEmail.js
@@ -5,12 +5,14 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'smtp.gmail.com',
     port: 456,
     secure: true, //enable ssl
     auth: {
         user: process.env.NODE_MAILER_EMAIL_USER,
         pass: process.env.NODE_MAILER_EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 10000,
 });
 
