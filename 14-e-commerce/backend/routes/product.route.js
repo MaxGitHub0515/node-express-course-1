a32b5bd41fc309a22542549eee19f4311b1e2c74
/ <105605213+MaxGitHub0515@users.noreply.github.com>
2026-01-01 15:46:16 +0100

    backend: product implementation & Auth

--- 14-e-commerce/backend/routes/product.route.js
+++ 14-e-commerce/backend/routes/product.route.js
@@ -1,7 +1,29 @@
 
 import express from "express";
 const router = express.Router();
+import { protectRoute, adminRoute } from "../middlewares/auth.middleware.js";
+import {
+    getAllProducts,
+    getFeaturedProducts,
+    createProduct,
+    deleteProduct,
+    getRecommendedProducts,
+    getProductsByCategory,
+    toggleFeaturedProduct
+} from "../controllers/product.controller.js";
 
-import {} from "../controllers/product.controller.js";
+router.get("/", protectRoute, adminRoute, getAllProducts);
+
+router.get("/featured", getFeaturedProducts);
+
+router.get("/recommendations", getRecommendedProducts);
+
+router.get("/category/:categoryId", getProductsByCategory);
+
+router.post("/", protectRoute, adminRoute, createProduct);
+
+router.delete("/:productId", protectRoute, adminRoute, deleteProduct);
+
+router.patch("/:productId", protectRoute, adminRoute, toggleFeaturedProduct);
 
 export default router;
