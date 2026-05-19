d0a63ee28fcb3c32a4fde2252e40888a6db45623
/ <105605213+MaxGitHub0515@users.noreply.github.com>
2026-05-19 19:33:11 +0200

    Uploaded archieved project

---
+++ 15-real-estate-moderate/client/src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLpao-oFLpFmAQGqLXwh-D-4OUaZSv5c8",
+  authDomain: "apps-41a71.firebaseapp.com",
+  projectId: "apps-41a71",
+  storageBucket: "apps-41a71.firebasestorage.app",
+  messagingSenderId: "864358898790",
+  appId: "1:864358898790:web:386c4f2e58da83400ee1a7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
+
+
